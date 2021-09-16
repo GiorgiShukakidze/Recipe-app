1251ac061e03c287428e177c84ab0ca0e6fdcb12
Giorgi Shukakidze <gshukakidze@gmail.com>
2021-09-16 13:40:49 +0200

    Initial Commit

---
+++ Recipe app/Recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_appApp.swift
+//  Recipe app
+//
+//  Created by Giorgi Shukakidze on 16.09.21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
